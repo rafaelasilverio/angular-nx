11b34c5b2dc592ed457034fe16842add24de5950
Rafaela Silvério <rafaelapetelin@gmail.com>
2024-03-20 15:44:13 -0300

    Config Compodoc e args dos Stories

--- modules/shared/design-system/src/lib/botao/botao.component.stories.ts
+++ modules/shared/design-system/src/lib/botao/botao.component.stories.ts
@@ -1,22 +1,22 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import type { Meta, StoryObj } from '@storybook/angular';
 import { BotaoComponent } from './botao.component';
 
 const meta: Meta<BotaoComponent> = {
-  title: 'Botão',
+  title: 'botao',
   tags: ['autodocs'],
   component: BotaoComponent,
   parameters: {
-    controls: { expanded: true },
+    // controls: { expanded: true },
     docs: {
       description: {
-        component: 'Este é um componente de botão utilizado para disparar ações.'
+        component: 'Este é um componente de botao utilizado para disparar ações.'
       }
     },
-    argTypes: {
-      texto: { control: 'text', },
-      desabilitado: { control: 'boolean' },
-      variante: { control: { type: 'select', options: ['primario', 'secundario'] } }
-    }
+    // argTypes: {
+    //   texto: { control: 'text', },
+    //   desabilitado: { control: 'boolean' },
+    //   variante: { control: { type: 'select', options: ['primario', 'secundario'] } }
+    // }
   }
 };
 export default meta;
@@ -27,100 +27,83 @@ export const Primario: Story = {
     texto: 'Action',
     desabilitado: false,
     variante: 'primario',
-    tema: ''
+    tema: 'azul'
   }
 };
 
 export const PrimarioDesabilitado: Story = {
   args: {
-    texto: 'Action',
+    ...Primario.args,
     desabilitado: true,
-    variante: 'primario',
   }
 }
 
 export const Secundario: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: false,
+    ...Primario.args,
     variante: 'secundario',
   }
 };
 
 export const SecundarioDesabilitado: Story = {
   args: {
-    texto: 'Action',
-    variante: 'secundario',
+    ...Secundario.args,
     desabilitado: true,
   }
 }
 
 export const Terciario: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: false,
+    ...Primario.args,
     variante: 'terciario',
   }
 };
 
 export const TerciarioDesabilitado: Story = {
   args: {
-    texto: 'Action',
+    ...Terciario.args,
     desabilitado: true,
-    variante: 'terciario',
   }
 };
 
 export const PrimarioVioleta: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: false,
-    variante: 'primario',
-    tema: 'tema-violeta'
+    ...Primario.args,
+    tema: 'violeta'
   }
 };
 
 export const PrimarioVioletaDesabilitado: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: true,
-    variante: 'primario',
-    tema: 'tema-violeta'
+    ...PrimarioVioleta.args,
+    desabilitado: true
   }
 };
 
 export const SecundarioVioleta: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: false,
+    ...PrimarioVioleta.args,
     variante: 'secundario',
-    tema: 'tema-violeta'
   }
 };
 
 export const SecundarioVioletaDesabilitado: Story = {
   args: {
-    texto: 'Action',
+    ...SecundarioVioleta.args,
     desabilitado: true,
-    variante: 'secundario',
-    tema: 'tema-violeta'
   }
 };
 
 export const TerciarioVioleta: Story = {
   args: {
-    texto: 'Action',
-    desabilitado: false,
+    ...PrimarioVioleta.args,
     variante: 'terciario',
-    tema: 'tema-violeta'
   }
 };
 
 export const TerciarioVioletaDesabilitado: Story = {
   args: {
-    texto: 'Action',
+    ...TerciarioVioleta.args,
     desabilitado: true,
-    variante: 'terciario',
-    tema: 'tema-violeta'
   }
 };
--- modules/shared/design-system/src/lib/botao/botao.component.ts
+++ modules/shared/design-system/src/lib/botao/botao.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 type variantes = 'primario' | 'secundario' | 'terciario';
-type temas = '' | 'tema-violeta';
+type temas = 'azul' | 'violeta';
 
 @Component({
   selector: 'myngapp-botao',
@@ -12,10 +12,13 @@ type temas = '' | 'tema-violeta';
   styleUrl: './botao.component.css',
 })
 export class BotaoComponent {
+  /**
+   * Texto botão
+   */
   @Input() texto = '';
   @Input() desabilitado = false;
   @Input() variante: variantes = 'primario';
-  @Input() tema: temas = '';
+  @Input() tema: temas = 'azul';
 
   obterVariante(variante: variantes, desabilitado: boolean, tema: temas): string {
     let classes = `${variante} ${tema}`;
