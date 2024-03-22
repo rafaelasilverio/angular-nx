d1753a584cd15575eaae622a67851aeb2b3e5206
Rafaela Silvério <rafaelapetelin@gmail.com>
2024-03-22 14:45:35 -0300

    table of contents

--- modules/shared/design-system/.storybook/preview.ts
+++ modules/shared/design-system/.storybook/preview.ts
@@ -1,18 +1,19 @@
-import type { Preview } from "@storybook/angular";
+import type { Preview } from '@storybook/angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 const preview: Preview = {
-    parameters: {
-        actions: { argTypesRegex: "^on [A-Z].*" },
-            controls: {
-                matchers: {
-                    color: /(background|color)$/i,
-                    date: /Dates$/,
-                },
-            },
+  parameters: {
+    docs: {toc: true},
+    actions: { argTypesRegex: '^on [A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Dates$/,
+      },
     },
+  },
 };
 
 export default preview;
